4512a182e1ac42d882ffda84a5bd83ca0f5bfe99
Xynnn007 <xynnn@linux.alibaba.com>
2023-02-17 16:24:35 +0800

    feat: add dockerfile for whole tdx stack
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- tool/build.sh
+++ tool/build.sh
@@ -4,11 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 
-if ! test -f sgx_rpm_local_repo.tgz; then
-    wget https://download.01.org/intel-sgx/sgx-dcap/1.15/linux/distro/Anolis86/sgx_rpm_local_repo.tgz
-fi
-
-
-tar zxvf sgx_rpm_local_repo.tgz
-
-docker build -t anolis86-tdx-dragonball --network host .
+docker build -t al3-tdx-qpl .
