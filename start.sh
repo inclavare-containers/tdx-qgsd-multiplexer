debaa0f8481419b1aee5f6f1e743ac7c062bc3b6
Xynnn007 <xynnn@linux.alibaba.com>
2025-09-29 16:35:45 +0800

    script: fix start script
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #! /bin/bash
-bash -c 'cat > /etc/sgx_default_qcnl.conf' << EOF
-# PCCS server address
-PCCS_URL=https://sgx-dcap-server.${REGION_ID}.aliyuncs.com/sgx/certification/v4/
-# To accept insecure HTTPS cert, set this option to FALSE
-USE_SECURE_CERT=TRUE
-EOF
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
